ed2c63e5778d8aa5e705f36bae12ca91230510ec
RyoichiNakai <elmobasketball0913@me.com>
2022-04-18 18:43:19 +0900

    split download

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/microsoft/vscode-remote-try-go
 
 go 1.14
+
+require golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"context"
+	"encoding/binary"
 	"flag"
-	"os"
-	"io"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
 
-	// "golang.org/x/sync/errgourp"
-
+	"golang.org/x/sync/errgroup"
 	//"downloader"
 )
 
 func main() {
-	// 実行時にURLを取得 
+	// 実行時にURLを取得
 	var url string
 	var divideNum int
 	var outputPath string
@@ -23,7 +25,7 @@ func main() {
 	flag.StringVar(&outputPath, "o", "output/sample.jpg", "output path (only jpg)")
 	flag.Parse()
 
-	/** 
+	/**
 	 * そのサイトが分割ダウンロードに対応しているかの確認
 	 * 対応している場合は、コンテンツのLengthを取ってくる
 	 */
@@ -31,25 +33,24 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	
+
 	// クライアントの設定
-	client := new(http.Client)
+	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	// Range Accessの存在チェック
-	if val, ok := res.Header["Accept-Ranges"]; ok {
-		fmt.Println(val)
+	if _, ok := res.Header["Accept-Ranges"]; !ok {
+		fmt.Println(ok)
 	}
 
 	contentLength, err := strconv.Atoi(res.Header["Content-Length"][0])
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(contentLength)
-	
+
 	/**
 	 * byte数を選択し、分割ダウンロード
 	 * これをgoroutineにて行う
@@ -57,46 +58,82 @@ func main() {
 
 	// 1アクセスごとにByte数を分割する
 	byteRangeArray := make([]string, divideNum)
-	accessRange := contentLength / divideNum
+	chank := contentLength / divideNum
 	for i := 0; i < divideNum; i++ {
-		begin := accessRange * i
-		if i == divideNum - 1 {
-			byteRangeArray[i] = strconv.Itoa(begin) + "-" + strconv.Itoa(contentLength)
+		begin := chank * i
+		if i == divideNum-1 {
+			byteRangeArray[i] = fmt.Sprintf("%d-%d", begin, contentLength)
 		} else {
-			end := accessRange * (i + 1) - 1
-			byteRangeArray[i] = strconv.Itoa(begin) + "-" + strconv.Itoa(end)
+			end := chank*(i+1) - 1
+			byteRangeArray[i] = fmt.Sprintf("%d-%d", begin, end)
 		}
 	}
 
-	// ファイルダウンロード
-	DownloadFile(outputPath, url)
+	/**
+	 * ファイルダウンロード処理
+	 */
+	eg, ctx := errgroup.WithContext(context.Background())
 
-}
+	byteDataArrary := make([][]byte, divideNum)
+	for i := 0; i < divideNum; i++ {
+		i := i
+		eg.Go(func() error {
+			fmt.Printf("[info] %d番目のダウンロード開始\n", i+1)
 
-func DownloadFile(filepath string, url string) error {
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				return err
+			}
+			req.Header.Set("Range", "bytes="+byteRangeArray[i])
 
-    req, err := http.NewRequest("GET", url, nil)
-    if err != nil {
-        return err
-    }
+			client := &http.Client{}
+			res, err := client.Do(req)
 
-		req.Header.Set("Range", "0-499")
-		// クライアントの設定
-		client := new(http.Client)
-		res, err := client.Do(req)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer res.Body.Close()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			data, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			byteDataArrary[i] = data
+			fmt.Printf("[info] %d番目のダウンロード完了\n", i+1)
+
+			return nil
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		fmt.Println(err)
+	}
+
+	/**
+	 * byte列をファイルに書き出す
+	 */
+
+	buf := &bytes.Buffer{}
+
+	for i := 0; i < divideNum; i++ {
+		err := binary.Write(buf, binary.BigEndian, byteDataArrary[i])
 		if err != nil {
 			fmt.Println(err)
 		}
-		defer res.Body.Close()
-		fmt.Println(res.Header)
+	}
 
-    file, err := os.Create(filepath)
-    if err != nil {
-        return err
-    }
-		defer file.Close()
+	file, err := os.Create(outputPath)
+	if err != nil {
+		fmt.Println(file)
+	}
+	defer file.Close()
 
-    _, err = io.Copy(file, res.Body)
-    return err
+	fmt.Println("[info] ファイルへ書き込み中です。。。")
+	file.Write(buf.Bytes())
+
+	fmt.Println("[info] ファイルへの書き込みが完了しました！")
 }
-
